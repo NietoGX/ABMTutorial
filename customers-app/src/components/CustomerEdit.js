c72e4b550e8a54b0191c9e81d78063d96373c9b1
NietoGX <al316458@uji.es>
2020-09-07 01:04:50 +0200

    105

--- customers-app/src/components/CustomerEdit.js
+++ customers-app/src/components/CustomerEdit.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {reduxForm, Field} from 'redux-form';
-import {connect} from "react-redux";
+import {Field, reduxForm} from 'redux-form';
 import {setPropsAsInitial} from "../helpers/setPropsAsInitial";
+const MyField = ({input,meta}) => (
+    <div>
+        <input {...input} type="text"/>
+            {
+                meta.touched && meta.error && <span>{meta.error}</span>
+            }
 
+    </div>
+);
+const isRequired = value => (
+    !value && "Este campo es requerido"
+);
 const CustomerEdit = ({name, dni, age}) => {
     return (
         <div>
             <h2>Edición de cliente</h2>
             <form action="">
                 <div><label htmlFor="name">Nombre: </label>
-                    <Field name ="name" component="input" type="text"></Field>
+                    <Field
+                        name="name"
+                        component={MyField}
+                        type="text"
+                        validate={isRequired}>
+                    </Field>
                 </div>
 
                 <div><label htmlFor="dni">Dni: </label>
-                    <Field name ="dni" component="input" type="text"></Field>
+                    <Field
+                        name="dni"
+                        component={MyField}
+                        type="text"
+                        validate={isRequired}>
+                    </Field>
 
                 </div>
 
                 <div><label htmlFor="age">Edad: </label>
-                    <Field name ="age" component="input" type="number"></Field>
+                    <Field
+                        name="age"
+                        component={MyField}
+                        type="number"
+                        validate={isRequired}>
+                    </Field>
 
                 </div>
             </form>
@@ -32,6 +57,6 @@ CustomerEdit.propTypes = {
     dni: PropTypes.string,
     age: PropTypes.number,
 };
-const CustomerEditForm= reduxForm({form: 'CustomerEdit'})(CustomerEdit);
+const CustomerEditForm = reduxForm({form: 'CustomerEdit'})(CustomerEdit);
 
 export default setPropsAsInitial(CustomerEditForm);
